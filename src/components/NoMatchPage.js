8da6bf1adf7bd213ed5374cf33f8c92bd5fb9aeb
Kevin Purcell <kevin91purcell@gmail.com>
2020-08-17 15:28:25 -0400

    updated nomatch page with return link. styled the nomatch page to match detaiils return link.

--- src/components/NoMatchPage.js
+++ src/components/NoMatchPage.js
@@ -1,12 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 
 const NoMatchPage = () => {
 
-
-
     return (
-      <div>
+      <div className="no-match">
+        <Link to='/'><p>Return Home</p></Link> 
         <h3>
          This page does not exist.
         </h3>
