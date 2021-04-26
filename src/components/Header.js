640e4e33179d3217a5a07996e136c5b94124c806
Kevin Purcell <kevin91purcell@gmail.com>
2021-04-26 16:20:34 -0400

    added higher quality netflix logo and switched to svg.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,15 @@
 import React from "react";
-import { Link } from 'react-router-dom';
-import netflix from '../images/netflix.png';
+import { Link } from "react-router-dom";
+// import netflix from '../images/netflix.png';
+import netflix from "../images/Netflix_logo_red.svg";
 import Search from "./Search";
 
-
 const Header = (props) => {
-
   return (
     <div className="App-header">
-      <Link to='/'><img alt='poster' src={netflix}/></Link> 
+      <Link to="/">
+        <img src={netflix} alt="Netflix logo" />
+      </Link>
       <Search search={props.search} />
     </div>
   );
