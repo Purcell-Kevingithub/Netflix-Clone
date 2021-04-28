b92d1e72e78a41070314d63fe48e284724998d0a
Kevin Purcell <kevin91purcell@gmail.com>
2021-04-28 16:49:15 -0400

    removed redunant divs and changed header to paragraph. changed background color to match other pages. changed display to flex and added arrow pseudo element to return link. added hover animation and fixed color scheme.

--- src/components/NoMatchPage.js
+++ src/components/NoMatchPage.js
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom";
 const NoMatchPage = () => {
   return (
     <div className="no-match">
+      <p className="no-match-heading">Page does not exist.</p>
       <Link to="/">
         <p>Return Home</p>
       </Link>
-      <h3>This page does not exist.</h3>
     </div>
   );
 };
