4ae21c1bda909e8741bd739741f089b710295ca0
Kevin Purcell <kevin91purcell@gmail.com>
2021-04-29 15:29:24 -0400

    destructed data for movie components and added height to movie height to images.

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -3,10 +3,11 @@ import { Link } from "react-router-dom";
 import placeholder from "../images/comingsoon.png";
 
 const Movie = ({ movie }) => {
+  const { original_title, poster_path } = movie;
   const poster =
     movie.poster_path === null
       ? placeholder
-      : `https://image.tmdb.org/t/p/w500/${movie.poster_path}`;
+      : `https://image.tmdb.org/t/p/w500/${poster_path}`;
   return (
     <div className="movie">
       <Link
@@ -16,8 +17,13 @@ const Movie = ({ movie }) => {
           state: { movie },
         }}
       >
-        <img alt={`The movie titled: ${movie.original_title}`} src={poster} />
+        <img
+          alt={`The movie titled: ${original_title}`}
+          src={poster}
+          className="movie-image"
+        />
       </Link>
+      <div></div>
     </div>
   );
 };
