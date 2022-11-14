04507218b55462ba6fe04504ca3a21da91a274f5
kevin purcell ubu <kevin91purcell@gmail.com>
2022-11-13 19:13:28 -0500

    add fallbacks. add dateformat function.

--- src/components/Moviedetails.js
+++ src/components/Moviedetails.js
@@ -3,41 +3,19 @@ import { movieDetailReducer, initDetail } from "../reducers/Reducers";
 import { Link } from "react-router-dom";
 import placeholder from "../images/comingsoon.png";
 import "../css/App.css";
+import dateFormat from "dateformat"
 
 import { faPlay } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Detail = (props) => {
-  const currentMovie = props.location.state.movie;
-  const currentId = props.location.state.movie.id;
+  const currentMovie = props?.location?.state?.movie;
+  const currentId = props?.location?.state?.movie?.id;
   const [trailer, trailerDispatch] = useReducer(movieDetailReducer, initDetail);
 
-  function formatDate() {
-    let currentDate = currentMovie.release_date.split("-");
-    let [year, month, day] = currentDate;
+  const releaseDate = currentMovie?.release_date
+  const formattedDate = dateFormat(releaseDate, "longDate",);
 
-    function formatMonth(month) {
-      return Number(month) >= 10 ? month : month.slice(1);
-    }
-
-    const months = [
-      "skip-this-month",
-      "January",
-      "February",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "December",
-    ];
-
-    return `${months[formatMonth(month)]}  ${day}, ${year}`;
-  }
 
   // use the passed in prop currentId to fetch the correct trailer
   useEffect(() => {
@@ -71,16 +49,11 @@ const Detail = (props) => {
     currentMovie.poster_path === null
       ? placeholder
       : `https://image.tmdb.org/t/p/w500/${currentMovie.poster_path}`;
-  //  this function will display nothing when trailer state is empty and then it will display an
-  // iframe once data is retrieved bc mapping will not cause an error vs directly accesing an index.
+
   const TrailersDisplay = () => {
     return (
       <div className="trailer">
-        {/* return null if video doesn't exist or first render when ajax requests hasn't completed. */}
-        {trailer.details.map((video) => {
-          if (!video) {
-            return null;
-          }
+        {trailer?.details.length > 0 && trailer.details.map((video) => {
           return (
             <iframe
               key={video.id}
@@ -99,14 +72,12 @@ const Detail = (props) => {
   const renderDetails = () => {
     return (
       <div>
-        {/* If loading and no error show loading message */}
-        {trailer.loading && !trailer.errorMessage ? (
+        {trailer?.loading && !trailer?.errorMessage ? (
           <span>loading... </span>
-        ) : // if there is an error messge display it
-          trailer.errorMessage ? (
-            <div className="errorMessage">{trailer.errorMessage}</div>
+        ) :
+          trailer?.errorMessage ? (
+            <div className="errorMessage">{trailer?.errorMessage}</div>
           ) : (
-            // This is the final case where not loading or an error. Movie payload is mapped over for each result and passed down as a movie prop
             <div className="details-container" style={{
               backgroundImage: `url(${poster})`,
               backgroundRepeat: "no-repeat",
@@ -115,10 +86,10 @@ const Detail = (props) => {
               <div className="details_trailer_container">
                 <div className="details">
                   <div className="details-heading">
-                    <h1 className="details-title">{currentMovie.original_title}</h1>
-                    <p className="details-date">Release Date: {formatDate()}</p>
+                    <h1 className="details-title">{currentMovie?.original_title}</h1>
+                    <p className="details-date">Release Date: {formattedDate}</p>
                   </div>
-                  <p className="overview">{currentMovie.overview}</p>
+                  <p className="overview">{currentMovie?.overview}</p>
                   <button className="play-button">
                     <FontAwesomeIcon icon={faPlay} size="sm" className="playIcon" />
                     Play
@@ -135,8 +106,7 @@ const Detail = (props) => {
     );
   };
 
-  // ADD RENDER LOGIC for diff reducer states on details.
-  return <div>{renderDetails()}</div>;
+  return renderDetails()
 };
 
 export default Detail;
